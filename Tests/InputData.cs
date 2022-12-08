0e1cd9abb4c11842412055bfdfc493c4cd048794
Damir Abdulin <damir.abdulin@tutanota.com>
2022-12-08 11:51:42 +0300

    ADD: Create simple tests

---
+++ Tests/InputData.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests;
+
+public class InputData
+{
+    public interface IService { }
+    public abstract class AbstractService { }
+    
+    
+    public class Service1 : AbstractService, IService { }
+    public class Service2 : AbstractService, IService { }
+
+    public class ServiceImpl : IService
+    {
+        public ServiceImpl(IRepository repository) { }
+    }
+
+    public interface IRepository { }
+
+    public class RepositoryImpl : IRepository
+    {
+        public RepositoryImpl() {}
+    }
+
+}
