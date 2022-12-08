f6c4a2438429953c7aba18b4649a025e624abc4e
Damir Abdulin <damir.abdulin@tutanota.com>
2022-12-08 11:36:14 +0300

    ADD: Create interface for di-container library

---
+++ DependencyInjectionContainer/DependenciesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace DependencyInjectionContainer;
+
+public class DependenciesConfiguration
+{
+    public void Register<TDependency, TImplementation>()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void Register<TDependency, TImplementation>(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DependencyInjectionContainer/DependencyProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace DependencyInjectionContainer;
+
+public class DependencyProvider
+{
+    private DependenciesConfiguration _dependencies;
+    
+    public DependencyProvider(DependenciesConfiguration dependencies)
+    {
+        _dependencies = dependencies;
+    }
+
+    public TDependency Resolve<TDependency>()
+    {
+        throw new NotImplementedException();
+    }
+
+    public TDependency Resolve<TDependency>(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
