3fa64e63ab709042b8f4cd6b7645ed423356d2f6
Damir Abdulin <damir.abdulin@tutanota.com>
2022-12-10 21:44:24 +0300

    ADD: Realized configuration interface

--- DependencyInjectionContainer/DependenciesConfiguration.cs
+++ DependencyInjectionContainer/DependenciesConfiguration.cs
@@ -1,14 +1,42 @@
-﻿namespace DependencyInjectionContainer;
+﻿using System.Collections.Concurrent;
+
+namespace DependencyInjectionContainer;
 
 public class DependenciesConfiguration
 {
+    private readonly ConcurrentDictionary<Type, ConcurrentBag<Type>> _lookupTable;
+
+    public DependenciesConfiguration()
+    {
+        _lookupTable = new ConcurrentDictionary<Type, ConcurrentBag<Type>>();
+    }
+
     public void Register<TDependency, TImplementation>()
     {
-        throw new NotImplementedException();
+        var typeDependency = typeof(TDependency);
+        if (typeDependency == null) 
+            throw new DependenciesConfigurationException($"typeof({nameof(typeDependency)}) returns null");
+        
+        var typeImplementation = typeof(TImplementation);
+        if (typeImplementation == null)
+            throw new DependenciesConfigurationException($"typeof({nameof(typeImplementation)}) returns null");
+
+        _lookupTable.TryAdd(typeDependency, new ConcurrentBag<Type>());
+        
+        _lookupTable[typeDependency].Add(typeImplementation);
     }
     
     public void Register<TDependency, TImplementation>(int id)
     {
         throw new NotImplementedException();
+    }
+
+    public ConcurrentBag<Type> GetImplementations(Type dependency)
+    {
+        if (!_lookupTable.TryGetValue(dependency, out var impl))
+            throw new DependenciesConfigurationException(
+                $"Configuration hasn't implementation for {nameof(dependency)}");
+        
+        return impl;
     }
 }
---
+++ DependencyInjectionContainer/DependenciesConfigurationException.cs
@@ -0,0 +1,12 @@
+﻿namespace DependencyInjectionContainer;
+
+public class DependenciesConfigurationException : Exception
+{
+    public DependenciesConfigurationException() { }
+
+    public DependenciesConfigurationException(string message)
+        : base(message) { }
+
+    public DependenciesConfigurationException(string message, Exception inner)
+        : base(message, inner) { }
+}
